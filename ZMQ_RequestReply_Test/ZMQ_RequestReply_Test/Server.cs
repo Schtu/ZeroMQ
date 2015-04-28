94b36e5ddf9bdbc90ca494d09053cd9f47a0af4a
Schtu <s4juesch@uni-trier.de>
2015-04-28 17:21:23 +0200

    Alles

---
+++ ZMQ_RequestReply_Test/ZMQ_RequestReply_Test/Server.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using NetMQ;
+
+namespace ZMQ_RequestReply_Test
+{
+	class MainClass : Client
+	{
+		public static void Main (string[] args)
+		{
+			NetMQContext context = NetMQContext.Create();
+			NetMQSocket server = context.CreateRequestSocket ();
+			server.Bind ("tcp://*:5555");
+			NetMQMessage message = new NetMQMessage ();
+			NetMQMessage getmass = new NetMQMessage ();
+			message.Append ("Hallo ");
+			message.Append(40);
+
+			Client client = new Client ();
+			Thread t = new Thread (new ThreadStart (client.runClient));
+			t.Start ();
+
+
+			while (true) {
+				server.SendMessage(message,false);
+				getmass = server.ReceiveMessage (false);
+				System.Console.WriteLine (getmass.Pop ().ConvertToString());
+				System.Console.WriteLine (getmass.Pop ().ConvertToInt32 ());
+				
+			}
+		}
+	}
+}
